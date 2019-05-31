b75a89cc801805fc66fd6a6375aff63be37ad3fb
Rohit Pandey <rohitpandey357@gmail.com>
2019-05-31 10:23:16 +0530

    deleted import statement of app.css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Index from './components/layout/Index';
 import Lyrics from './components/tracks/Lyrics';
 import Provider from './context';
 
-import './App.css';
-
 class App extends React.Component {
   render () {
     return (
